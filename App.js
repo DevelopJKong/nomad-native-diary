ddaaf131e9d19622d3e8ed4db0ff5ff32cc5246f
DevelopJKong <ybaram40@gmail.com>
2022-10-10 08:51:08 +0900

    #5.4 Writing Objects 완료 | 5.1 차근차근 다시 해볼 예정

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Realm from 'realm';
+import { DBContext } from './context';
+
+const FeelingSchema = {
+  name: 'Feeling',
+  properties: {
+    _id: 'int',
+    emotion: 'string',
+    message: 'string',
+  },
+  primaryKey: '_id',
+};
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+  const [realm, setRealm] = useState(null);
+  useEffect(() => {
+    const startLoading = async () => {
+      const connection = await Realm.open({
+        path: 'nomadDiaryDB',
+        schema: [FeelingSchema],
+      });
+      setRealm(connection);
+    };
+    startLoading();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <DBContext.Provider value={realm}>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style='auto' />
+      </View>
+    </DBContext.Provider>
   );
 }
 
---
+++ context.js
@@ -0,0 +1,1 @@
+export const DBContext = React.createContext();
