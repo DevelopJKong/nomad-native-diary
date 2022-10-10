8267b73930b7c0740fdc405aa604d3ee44331284
DevelopJKong <ybaram40@gmail.com>
2022-10-10 12:08:55 +0900

    #5.5 Reading Objects 완료

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components/native';
 import colors from '../colors';
 import { Ionicons } from '@expo/vector-icons';
+import { useDB } from '../context';
+import { FlatList } from 'react-native';
 
 const View = styled.View`
   flex: 1;
@@ -31,10 +33,59 @@ const Btn = styled.TouchableOpacity`
   box-shadow: 1px 1px 3px rgba(41, 30, 95, 0.2);
 `;
 
+const Record = styled.View`
+  background-color: ${colors.cardColor};
+  align-items: center;
+  flex-direction: row;
+  padding: 10px 20px;
+  border-radius: 10px;
+`;
+
+const Emotion = styled.Text`
+  font-size: 26px;
+  margin-right: 10px;
+`;
+
+const Message = styled.Text`
+  font-size: 20px;
+`;
+
+const Separator = styled.View`
+  height: 10px;
+`;
+
 const Home = ({ navigation: { navigate } }) => {
+  const realm = useDB();
+  const [feelings, setFeelings] = useState([]);
+  useEffect(() => {
+    if (realm) {
+      const feelingObject = realm.objects('Feeling');
+      setFeelings(feelingObject);
+      feelingObject.addListener(() => {
+        const feelingObject = realm.objects('Feeling');
+        setFeelings(feelingObject);
+      });
+      return () => {
+        feelingObject.removeAllListeners();
+      };
+    }
+  }, []);
+
   return (
     <View>
       <Title>My journal</Title>
+      <FlatList
+        data={feelings}
+        ItemSeparatorComponent={Separator}
+        contentContainerStyle={{ paddingVertical: 10 }}
+        keyExtractor={(feeling) => feeling._id + ''}
+        renderItem={({ item }) => (
+          <Record>
+            <Emotion>{item.emotion}</Emotion>
+            <Message>{item.message}</Message>
+          </Record>
+        )}
+      />
       <Btn onPress={() => navigate('Write')}>
         <Ionicons name='add' color={colors.textColor} size={40} />
       </Btn>
--- screens/Write.js
+++ screens/Write.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components/native';
 import colors from '../colors';
+import { useDB } from '../context';
 
 const View = styled.View`
   background-color: ${colors.bgColor};
@@ -60,7 +61,8 @@ const EmotionText = styled.Text`
 
 const emotions = ['🤯', '🤬', '🤗', '🥰', '😊', '🤩'];
 
-const Write = () => {
+const Write = ({ navigation: { goBack } }) => {
+  const realm = useDB();
   const [selectedEmotion, setEmotion] = useState(null);
   const [feelings, setFeelings] = useState('');
   const onChangeText = (text) => setFeelings(text);
@@ -69,6 +71,14 @@ const Write = () => {
     if (feelings === '' || selectedEmotion == null) {
       return Alert.alert('Please complete form.');
     }
+    realm.write(() => {
+      realm.create('Feeling', {
+        _id: Date.now(),
+        emotion: selectedEmotion,
+        message: feelings,
+      });
+    });
+    goBack();
   };
   return (
     <View>
